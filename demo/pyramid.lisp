2829f0004743aaa08815c929371e10a2e680df71
death <death@adeht.org>
2020-07-10 11:39:13 +0300

    cosmetix

--- demo/pyramid.lisp
+++ demo/pyramid.lisp
@@ -13,63 +13,54 @@
 
 (defmethod init-demo ((demo pyramid-demo))
   (reset-shape-id-counter)
-  (setf (world demo) (make-world :iterations 20
-                            :gravity (vec 0 -300)))
-  (resize-world-active-hash (world demo) 40.0 2999)
-  (resize-world-static-hash (world demo) 40.0 999)
-  (setf (demo-floor demo) (make-body :actor :not-grabbable
-                                     :shapes (list (make-segment (vec -600 -240)
-                                                                 (vec 600 -240)
-                                                                 :restitution 1
-                                                                 :friction 1))))
-  (world-add-body (world demo) (demo-floor demo))
-  (let ((friction 0.6)
-        (verts (list (vec -3 -20)
-                     (vec -3 20)
-                     (vec 3 20)
-                     (vec 3 -20))))
-    (loop
-       with n = 9
-       for i from 1 to n
-       for offset = (vec (- (/ (* i 60) 2))
-                         (* (- n i) 52))
-       do (loop
-             for j from 0 below i do
+  (let ((world (make-world :iterations 20 :gravity (vec 0 -300))))
+    (resize-world-active-hash world 40.0 2999)
+    (resize-world-static-hash world 40.0 999)
+    (setf (demo-floor demo)
+          (make-body :actor :not-grabbable
+                     :shapes (list (make-segment (vec -600 -240)
+                                                 (vec 600 -240)
+                                                 :restitution 1
+                                                 :friction 1))))
+    (world-add-body world (demo-floor demo))
+    (let ((friction 0.6)
+          (verts (list (vec -3 -20)
+                       (vec -3 20)
+                       (vec 3 20)
+                       (vec 3 -20))))
+      (loop with n = 9
+            for i from 1 to n
+            for offset = (vec (- (/ (* i 60) 2))
+                              (* (- n i) 52))
+            do (loop for j from 0 below i
+                     do (mapc (lambda (body)
+                                (attach-shape (make-poly verts :friction friction) body)
+                                (world-add-body world body))
+                              (nconc (list (make-body :mass 1
+                                                      :position (vec+ (vec (* j 60)
+                                                                           -220)
+                                                                      offset))
+                                           (make-body :mass 1
+                                                      :position (vec+ (vec (* j 60)
+                                                                           -197)
+                                                                      offset)
+                                                      :angle (/ pi 2)))
+                                     (unless (= j (1- i))
+                                       (list (make-body :mass 1
+                                                        :position (vec+ (vec (+ (* j 60) 30)
+                                                                             -191)
+                                                                        offset)
+                                                        :angle (/ pi 2)))))))
                (mapc (lambda (body)
-                       (attach-shape (make-poly verts :friction friction)
-                                     body)
-                       (world-add-body (world demo) body))
-                     (nconc
-                      (list (make-body
-                             :mass 1
-                             :position (vec+ (vec (* j 60) -220)
-                                             offset))
-                            (make-body
-                             :mass 1
-                             :position (vec+ (vec (* j 60) -197)
-                                             offset)
-                             :angle (/ pi 2)))
-                      (unless (= j (1- i))
-                        (list (make-body
-                               :mass 1
-                               :position (vec+ (vec (+ (* j 60) 30)
-                                                    -191)
-                                               offset)
-                               :angle (/ pi 2)))))))
-         (mapc (lambda (body)
-                 (attach-shape (make-poly verts :friction friction)
-                               body)
-                 (world-add-body (world demo) body))
-               (list (make-body
-                      :mass 1
-                      :position (vec+ (vec -17
-                                           -174)
-                                      offset))
-                     (make-body
-                      :mass 1
-                      :position (vec+ (vec (+ (* (1- i) 60) 17)
-                                           -174)
-                                      offset))))))
-  (world demo))
+                       (attach-shape (make-poly verts :friction friction) body)
+                       (world-add-body world body))
+                     (list (make-body :mass 1
+                                      :position (vec+ (vec -17 -174)
+                                                      offset))
+                           (make-body :mass 1
+                                      :position (vec+ (vec (+ (* (1- i) 60) 17)
+                                                           -174)
+                                                      offset))))))
+    world))
 
 (provide-demo 'pyramid-demo)
